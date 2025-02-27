8d8569506cbb9a244c54b51f4d74414294b9a084
alaaMandour <alaaMandour2024@gmail.com>
2025-02-27 21:28:40 +0200

    Initial commit
    # Conflicts:
    #	README.md

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'layouts/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 132, 0, 51)),
+        useMaterial3: true
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
