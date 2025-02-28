04430c35e5dcc53291b57f3d7aba4792b4c82894
alaaMandour <alaaMandour2024@gmail.com>
2025-03-01 00:34:17 +0200

    handling meals data & models

--- lib/layouts/home_screen.dart
+++ lib/layouts/home_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/widgets/category_item.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('data'),),
+      body: GridView.builder(
+        itemBuilder: (BuildContext context, int index) => CategoryItem(category: availableCategories[index],),
+        itemCount: availableCategories.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 15,
+            crossAxisSpacing: 15,
+            childAspectRatio: 3/2
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/layouts/meal_screen.dart';
+
+import '../models/category.dart';
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.category});
+
+  final Category category; 
+  
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap:() {
+        Navigator.of(context).push(
+            MaterialPageRoute(
+                builder: (context) => MealScreen(),
+            )
+        );
+      },
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(22),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.54),
+                category.color.withOpacity(0.9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          )
+        ),
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          )
+      ),
+    );
+  }
+}
